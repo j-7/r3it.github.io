3c66954f347b1a4ec27876904da5d8a201cec8eb
K Nishijima <nishijima@r3it.com>
2015-06-29 17:44:26 +0900

    rebuilding site 2015年 6月29日 月曜日 17時44分26秒 JST

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 
-$(document).ready(function(){/* activate scrollspy menu */
-$('body').scrollspy({
-  target: '#navbar-collapsible',
-  offset: 50
-});
+jQuery(document).ready(function(){/* activate scrollspy menu */
+    jQuery('body').scrollspy({
+        target: '#navbar-collapsible',
+        offset: 50
+    });
 
-/* smooth scrolling sections */
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 50
-        }, 1000);
-        return false;
-      }
-    }
-});
+    /* smooth scrolling sections */
+    jQuery('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = jQuery(this.hash);
+            target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                jQuery('html,body').animate({
+                    scrollTop: target.offset().top - 50
+                }, 1000);
+                return false;
+            }
+        }
+    });
 
 });
